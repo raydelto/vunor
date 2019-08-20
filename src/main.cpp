e5cbcd1cefbb06a5d245a0cb3941a0d3faca9402
Raydelto Hernandez <raydelto@gmail.com>
2019-08-20 01:09:26 -0300

    Adding a second triange to the scene

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,13 @@ int main()
     triangle->SetPosition({400,300});
     triangle->SetColor({0.0f, 1.0f, 0.0f});
     scene->Add(triangle);
+
+    std::shared_ptr<Triangle> triangle2 = std::make_shared<Triangle>();
+    triangle2->SetSize({100,100});
+    triangle2->SetPosition({0,0});
+    triangle2->SetColor({0.0f, 0.0f, 1.0f});
+    scene->Add(triangle2);
+
     scene->RenderLoop();
     return 0;
 }
