d7ffc4e5063768853e02d034a7791063a2520ac9
Raydelto Hernandez <raydelto@gmail.com>
2020-06-26 02:14:36 -0300

    Rendering many triangles

--- src/main.cpp
+++ src/main.cpp
@@ -15,27 +15,27 @@ int main()
     std::shared_ptr<Triangle> triangle = std::make_shared<Triangle>();
     triangle->SetSize({200,200});
     triangle->SetPosition(position);
-    triangle->SetColor({1.0f, 0.0f, 0.0f});
+    triangle->SetColor({0.5f, 0.35f, 0.0f});
     scene->Add(triangle);
 
-    // std::shared_ptr<Triangle> triangle2 = std::make_shared<Triangle>();
-    // triangle2->SetSize({100,100});
-    // triangle2->SetPosition({0.0f,0.0f});
-    // triangle2->SetColor({0.0f, 0.0f, 1.0f});
-    // scene->Add(triangle2);
+    std::shared_ptr<Triangle> triangle2 = std::make_shared<Triangle>();
+    triangle2->SetSize({100,100});
+    triangle2->SetPosition({0.0f,0.0f});
+    triangle2->SetColor({0.0f, 0.0f, 1.0f});
+    scene->Add(triangle2);
 
-    // std::shared_ptr<Triangle> triangle3 = std::make_shared<Triangle>();
-    // triangle3->SetSize({100,100});
-    // triangle3->SetPosition({200.0f,200.0f});
-    // triangle3->SetColor({1.0f, 0.0f, 0.0f});
-    // scene->Add(triangle3);
+    std::shared_ptr<Triangle> triangle3 = std::make_shared<Triangle>();
+    triangle3->SetSize({100,100});
+    triangle3->SetPosition({200.0f,200.0f});
+    triangle3->SetColor({1.0f, 0.0f, 0.0f});
+    scene->Add(triangle3);
 
 
-    // std::shared_ptr<Triangle> triangle4 = std::make_shared<Triangle>();
-    // triangle4->SetSize({40,40});
-    // triangle4->SetPosition({600.0f,400.0f});
-    // triangle4->SetColor({1.0f, 1.0f, 0.0f});
-    // scene->Add(triangle4);
+    std::shared_ptr<Triangle> triangle4 = std::make_shared<Triangle>();
+    triangle4->SetSize({40,40});
+    triangle4->SetPosition({600.0f,400.0f});
+    triangle4->SetColor({1.0f, 1.0f, 0.0f});
+    scene->Add(triangle4);
 
 
     scene->RenderLoop();
